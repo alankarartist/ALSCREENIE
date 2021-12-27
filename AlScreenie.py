2d36a8fdbfe881e1b3a3176bfa08f44d62ba7e24
alankarartist <alankarartist@gmail.com>
2021-12-27 11:37:44 +0530

    AlScreenie

--- AlScreenie.py
+++ AlScreenie.py
@@ -1,9 +1,8 @@
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtGui import *
+from PyQt5.QtGui import QIcon
 import datetime
-import cv2 
+import cv2
 import numpy as np
-from dateutil.relativedelta import relativedelta
 from PIL import ImageGrab
 from UI.AlScreenieUI import Ui_Form
 from AlGUILoop.AlGUILoop import AlGUILoop, stopLoop
@@ -12,13 +11,15 @@ import os
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 
+
 class MoveWidget(QtWidgets.QWidget):
     def __init__(self):
         super(MoveWidget, self).__init__()
 
     def mousePressEvent(self, event):
         if event.button() == QtCore.Qt.LeftButton:
-            self.dragPosition = event.globalPos() - self.frameGeometry().topLeft()
+            self.dragPosition = (event.globalPos() -
+                                 self.frameGeometry().topLeft())
             event.accept()
 
     def mouseMoveEvent(self, event):
@@ -26,12 +27,14 @@ class MoveWidget(QtWidgets.QWidget):
             self.move(event.globalPos() - self.dragPosition)
             event.accept()
 
+
 class AlScreenie(MoveWidget, Ui_Form):
     def __init__(self):
         super(AlScreenie, self).__init__()
-        self.setWindowIcon(QIcon(os.path.join(cwd+'\\UI\\icons', 'alscreenie.png')))
+        self.setWindowIcon(QIcon(os.path.join(cwd+'\\UI\\icons',
+                                              'alscreenie.png')))
         self.setupUi(self)
-        self.setGeometry(1650,200,250,50)
+        self.setGeometry(1650, 200, 250, 50)
         sizeObject = QtWidgets.QDesktopWidget().screenGeometry(-1)
         self.W = sizeObject.width()
         self.H = sizeObject.height()
@@ -39,22 +42,23 @@ class AlScreenie(MoveWidget, Ui_Form):
         self.pushButton_2.clicked.connect(self.stop)
         self.pushButton_3.clicked.connect(self.exitRecorder)
         self.start()
-    
-    @AlGUILoop   
+
+    @AlGUILoop
     def record(self):
         timeStamp = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
         fileName = f'Screen Recording {timeStamp}.mp4'
         filePath = os.path.join(cwd+'\\AlScreenie', fileName)
         fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-        capturedVideo = cv2.VideoWriter(filePath, fourcc, 20.0, (self.W, self.H))
+        capturedVideo = cv2.VideoWriter(filePath, fourcc, 20.0,
+                                        (self.W, self.H))
         while(True):
-            frame = ImageGrab.grab(bbox=(0,0,self.W,self.H))
+            frame = ImageGrab.grab(bbox=(0, 0, self.W, self.H))
             frame = np.array(frame)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             self.label.setText('RECORDING')
             capturedVideo.write(frame)
             yield 0.001
-        
+
     def start(self):
         self.generator = self.record()
 
@@ -66,6 +70,7 @@ class AlScreenie(MoveWidget, Ui_Form):
     def exitRecorder(self):
         self.stop()
         sys.exit()
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
